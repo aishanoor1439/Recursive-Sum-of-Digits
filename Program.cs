e65ba9c3f4ec9378d13e1e88440e7c075550cc18
aishanoor1439 <aisha.noor1439@gmail.com>
2025-01-21 09:43:52 +0100

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Sum_of_Digits
+{
+    internal class Program
+    {
+        static int SumOfDigits(int number)
+        {
+            if (number == 0)
+                return 0;
+            return (number % 10) + SumOfDigits(number / 10);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a non-negative integer to calculate the sum of its digits: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if (input < 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a non-negative integer.");
+            }
+            else
+            {
+                int result = SumOfDigits(input);
+                Console.WriteLine($"The sum of the digits of {input} is: {result}");
+            }
+        }
+    }
+}
